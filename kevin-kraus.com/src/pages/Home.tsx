35a085fc0886f0e870ef6b0191814ebd530c4b52
Kevin Kraus <kevin.kraus@globalside.com>
2021-07-25 19:00:10 +0200

    feat: optimize imports
    Signed-off-by: Kevin Kraus <kevin.kraus@globalside.com>

--- kevin-kraus.com/src/pages/Home.tsx
+++ kevin-kraus.com/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {MainCard} from "../components/MainCard/MainCard";
 import {useTranslation} from "react-i18next";
 import {PageContent} from "../components/PageContent/PageContent";
 import {HeaderBar} from "../components/HeaderBar/HeaderBar";
